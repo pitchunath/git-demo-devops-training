7c916add2ba1145b45dacc8af8cbfea376e4b1ca
Pitchu Nath <pitchunath123@gmail.com>
2021-05-24 10:45:53 +0000

    added new feature

--- demo1.java
+++ demo1.java
@@ -3,6 +3,6 @@ public class demo1 {
 
   public static void main(String[] args) {
   
-    System.out.println("This is demo1 of developer A");
+    System.out.println("This is demo1 of developer Aand feauture");
   }
 }
