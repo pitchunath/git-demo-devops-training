d538812a9160fbc04d5a16a4094591a441f90031
Pitchu Nath <pitchunath123@gmail.com>
2021-05-24 10:35:12 +0000

    changes from developer B

--- demo2.java
+++ demo2.java
@@ -1,5 +1,5 @@
 public class demo2 {
   public static void main(String[] args) {
-    System.out.println("This is demo2 of developer A");
+    System.out.println("This is demo2 of developer B");
   }
 }
