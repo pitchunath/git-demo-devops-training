dfa383ebc2d3c835d5ee23a3628cb46a8f904ed1
Pitchu Nath <pitchunath123@gmail.com>
2021-05-25 10:00:47 +0000

    kane changes added

--- demo1.java
+++ demo1.java
@@ -3,6 +3,6 @@ public class demo1 {
 
   public static void main(String[] args) {
   
-    System.out.println("This is demo1 of developer Aand feauture");
+    System.out.println("This is demo1 developed by kane");
   }
 }
